f0729e3a035cf22b2ea9ff78c4caca740d3128e6
Muhammad Henry Aditya <adityahenry2@gmail.com>
2024-07-17 19:58:11 +0800

    [LAD-3] Fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,7 +12,7 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('https://149.129.195.54/LAD/public');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
